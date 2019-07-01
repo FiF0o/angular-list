5664b867c3867c707993259a1e35cd036e90f0e1
FiF0o <jonathan.lazarini@wipro.com>
2019-07-01 15:45:36 +0100

    add material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppMaterialModule } from './app-material.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
+
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    BrowserAnimationsModule
+    AppMaterialModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
