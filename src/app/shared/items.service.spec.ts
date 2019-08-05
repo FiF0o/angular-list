92123c08cdc2dc597d4e56f6792fe4ebac49661e
FiF0o <jonathan.lazarini@wipro.com>
2019-08-05 14:50:24 +0100

    add basic unit test for service

---
+++ src/app/shared/items.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed, inject } from '@angular/core/testing'
+import { HttpClient } from '@angular/common/http';
+
+import { ItemsService } from './items.service';
+
+class HttpStub {}
+
+describe('Service: Items', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ItemsService,
+        {
+          provide: HttpClient, 
+          useClass: HttpStub
+        }
+      ]
+    })
+  })
+
+  it('should exist', inject([ItemsService], (service: ItemsService) => {
+    expect(service).toBeTruthy();
+  }))
+})
