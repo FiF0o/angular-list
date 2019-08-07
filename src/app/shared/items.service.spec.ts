e5681fa800af63f971863974a6a02b0b9eb93f0e
FiF0o <jonathan.lazarini@wipro.com>
2019-08-07 14:14:35 +0100

    add test for get servvice and observable test util

--- src/app/shared/items.service.spec.ts
+++ src/app/shared/items.service.spec.ts
@@ -2,23 +2,32 @@ import { TestBed, inject } from '@angular/core/testing'
 import { HttpClient } from '@angular/common/http';
 
 import { ItemsService } from './items.service';
+import { ItemInterface } from './item.model';
 
-class HttpStub {}
+import { asyncData } from '../testing/async-observable-helpers'
+
+let httpClientSpy: { get: jasmine.Spy };
+let itemsService: ItemsService;
 
 describe('Service: Items', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        ItemsService,
-        {
-          provide: HttpClient, 
-          useClass: HttpStub
-        }
-      ]
-    })
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+    itemsService = new ItemsService(<any> httpClientSpy)
   })
 
-  it('should exist', inject([ItemsService], (service: ItemsService) => {
-    expect(service).toBeTruthy();
-  }))
+  it('should return expected items (HttpClient called once)', () => {
+    const expectedItems: ItemInterface[] = [
+      {id: 1},
+      {id: 2}
+    ]
+    httpClientSpy.get.and.returnValue(asyncData(expectedItems))
+
+    itemsService.getAll().subscribe(
+      items => expect(items).toEqual(expectedItems),
+      fail
+    )
+
+    expect(httpClientSpy.get.calls.count()).toBe(1)
+  })
+
 })
